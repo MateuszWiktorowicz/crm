b4c4c68e82b8e3fb324025349def079f0f10763b
Mateusz Wiktorowicz <wiktorowicz.mat@gmail.com>
2025-02-10 16:00:24 +0100

    add pinia

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,15 +1,16 @@
 import { createRouter } from 'vue-router';
 import { createWebHistory } from 'vue-router';
 import DefaultLayout from './Layouts/DefaultLayout.vue';
-import Home from './Pages/Home.vue';
 import MyImages from './Pages/MyImages.vue';
 import Login from './Pages/Login.vue';
-import Signup from './Pages/Signup.vue';
+import Signup from './Pages/Employees/CreateForm.vue';
 import NotFound from './Pages/NotFound.vue';
 import Dashboard from './Pages/Dashboard.vue';
 import Customers from './Pages/Customers.vue';
 import Offers from './Pages/Offers.vue';
-import Employees from './Pages/Employees.vue';
+import Employees from './Pages/Employees/Employees.vue';
+import useUserStore from './store/user';
+
 
 const routes = [
     {
@@ -21,8 +22,17 @@ const routes = [
             {path: '/klienci', name: 'Customers', component: Customers},
             {path: '/oferty', name: 'Offers', component: Offers},
             {path: '/pracownicy', name: 'Employees', component: Employees},
-
-        ]
+        ],
+        beforeEnter: async (to, from, next) => {
+            try {
+                const userStore = useUserStore();
+                await userStore.fetchUser();
+                next();
+            } catch (error) {
+                console.error('Failed to fetch user', error);
+                next('/login');
+        }
+    }
     },
     {
         path: '/login',
---
+++ frontend/src/store/user.js
@@ -0,0 +1,69 @@
+import { defineStore } from "pinia";
+import axiosClient from "../axios.js";
+
+const useUserStore = defineStore('user', {
+    state: () => ({
+        users: [],
+        user: {
+            name: '',
+            email: '',
+            password: '',
+            password_confirmation: '',
+            marker: '',
+            roles: [],
+        },
+        isModalOpen: false,
+        errors: {},
+    }),
+    actions: {
+        async fetchUser() {
+            try {
+                const response = await axiosClient.get('/api/pracownicy');
+                this.user = response.data;
+            } catch (error) {
+                console.error("Błąd pobierania użytkownika", error);
+            }
+        },
+        async fetchUsers() {
+            try {
+                const response = await axiosClient.get('api/pracownicy');
+                this.users = response.data;
+            } catch (error) {
+                console.log(error);
+            }
+        },   
+        async saveUser() {
+            try {
+                if (this.user.id) {
+                    await axiosClient.put(`/api/pracownicy/${this.user.id}`, this.user);
+                } else {
+                    await axiosClient.post("/api/pracownicy", this.user);
+                }
+                this.isModalOpen = false;
+                await this.fetchUsers();
+                this.errors = {};
+            } catch (error) {
+                this.errors = error.response?.data?.errors ?? {};
+            }
+        },
+        async deleteUser(userId) {
+            try {
+                if (confirm("Czy na pewno chcesz usunąć tego użytkownika?")) {
+                    await axiosClient.delete(`/api/pracownicy/${userId}`);
+                    await this.fetchUsers();
+                }
+            } catch (error) {
+                console.error("Błąd podczas usuwania użytkownika:", error);
+            }
+        },
+        openModal(user = null) {
+            this.user = user || { name: "", email: "", password: "", password_confirmation: "", marker: "", roles: [] };
+            this.isModalOpen = true;
+        },
+        closeModal() {
+            this.isModalOpen = false;
+        }
+    },
+});
+
+export default useUserStore;
