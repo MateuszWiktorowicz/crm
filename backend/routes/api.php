f89b8038cb1dc2e08700dc4916a717ffabda13d7
Mateusz Wiktorowicz <wiktorowicz.mat@gmail.com>
2025-02-10 15:59:43 +0100

    add apis endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{CustomerController, UserController};
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    
+    Route::apiResource('/klienci', CustomerController::class)
+    ->only(['index', 'store', 'destroy']);
+
+    Route::apiResource('/pracownicy', UserController::class)
+    ->only(['index', 'store']);
+
+    Route::put('/pracownicy/{user}', [UserController::class, 'edit']);
+    Route::delete('/pracownicy/{user}', [UserController::class, 'delete']);
 });
