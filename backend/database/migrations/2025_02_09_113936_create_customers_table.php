080ac7b87a8e75dc8f59d821f1a7675b41407235
Mateusz Wiktorowicz <wiktorowicz.mat@gmail.com>
2025-02-10 15:59:15 +0100

    add migrations

---
+++ backend/database/migrations/2025_02_09_113936_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 255)->unique();
+            $table->string('name', 255);
+            $table->string('nip', 10)->unique();
+            $table->string('zip_code', 10)->nullable();
+            $table->string('city', 255)->nullable();
+            $table->string('address', 255)->nullable();
+            $table->string('saler_marker', 10)->nullable();
+            $table->string('description', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
