d24ba344a2c1e996419c1d09142d85c0e7825d6a
Mateusz Wiktorowicz <wiktorowicz.mat@gmail.com>
2025-02-13 12:32:00 +0100

    OfferDetail model nad migration

---
+++ backend/app/Models/OfferDetail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class OfferDetail extends Model
+{
+    protected $fillable = [
+        'offer_id',
+        'tool_geometry_id',
+        'tool_quantity',
+        'tool_discount',
+        'tool_net_price',
+        'tool_gross_price',
+        'coating_price_id',
+        'coating_quantity',
+        'coating_discount',
+        'coating_net_price',
+        'coating_gross_price',
+    ];
+
+    public function offer(): BelongsTo
+    {
+        return $this->belongsTo(Offer::class);
+    }
+
+    public function toolGeometry(): BelongsTo
+    {
+        return $this->belongsTo(ToolGeometry::class);
+    }
+
+    public function coatingPrice(): BelongsTo
+    {
+        return $this->belongsTo(CoatingPrice::class, 'coating_price_id');
+    }
+}
