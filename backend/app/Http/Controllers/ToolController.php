a467d466444da3cf85e2de6bead1646faedf9a05
Mateusz Wiktorowicz <wiktorowicz.mat@gmail.com>
2025-02-13 15:43:04 +0100

    get tools

--- backend/app/Http/Controllers/ToolController.php
+++ backend/app/Http/Controllers/ToolController.php
@@ -2,17 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ToolGeometry;
 use Illuminate\Http\Request;
 use App\Models\ToolType;
+use Illuminate\Support\Facades\DB;
 
 class ToolController extends Controller
 {
     public function index()
     {
         $toolTypes = ToolType::all();
+        $tools = DB::table('tool_geometries')
+            ->join('tool_types', 'tool_geometries.id_tool_type', '=', 'tool_types.id')
+            ->select(
+                'tool_geometries.id',
+                'tool_geometries.flutes_number',
+                'tool_geometries.diameter', 
+                'tool_types.tool_type_name',
+                DB::raw('tool_geometries.face_grinding_time * 5 as face_grinding_price'),
+                DB::raw('tool_geometries.periphery_grinding_time_2d_tool * 5 as periphery_grinding_price')
+                )
+            ->get();
 
         return response()->json([
-            'toolTypes' => $toolTypes,
+            'toolTypes' => $toolTypes, 
+            'tools' => $tools,
         ]);
     }
 }
