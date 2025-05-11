bb78187008a899d171968773bf73f7b67d7f1f41
Mateusz Wiktorowicz <wiktorowicz.mat@gmail.com>
2025-05-11 15:14:24 +0200

    change file name

--- backend/database/migrations/2025_05_11_130504_update_coating_price_id_nullable_in_offer_detailsv2.php
+++ backend/database/migrations/2025_05_11_130504_update_coating_price_id_nullable_in_offer_detailsv2.php
@@ -4,7 +4,7 @@
     use Illuminate\Database\Schema\Blueprint;
     use Illuminate\Support\Facades\Schema;
     
-    class UpdateCoatingPriceIdAsNullableInOfferDetails extends Migration
+    class UpdateCoatingPriceIdAsNullableInOfferDetailsv2 extends Migration
     {
         public function up()
         {
